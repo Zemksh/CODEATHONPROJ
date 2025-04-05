f9c38668ad6755915150e1846a74abab384376a5
adilzainulsyed <nameisadil02@gmail.com>
2025-04-05 16:15:03 +0530

    the mvp

---
+++ app.py
@@ -0,0 +1,15 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def dashboard():
+    user = {
+        'name': 'Adil',
+        'photo_url': '../static/images/profilepic.jpg'
+    }
+    return render_template('dashboard.html', user=user)
+
+if __name__ == '__main__':
+    app.run(debug=True)
